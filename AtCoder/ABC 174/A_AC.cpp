d7ea58bb7c284ad4f9e3cba3cb82c751784aeded
4hn6 <gb.kim@postech.ac.kr>
2020-08-11 19:53:50 +0900

    ABC 174 source code

---
+++ AtCoder/ABC 174/A_AC.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+
+    if(x >= 30) printf("Yes");
+    else printf("No");
+
+    return 0;
+}
